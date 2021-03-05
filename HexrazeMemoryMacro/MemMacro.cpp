cc679a09b07198e0ebbb66e0440211e0011b3bd0
Barracuda1900 <76521305+Barracuda1900@users.noreply.github.com>
2021-03-05 18:48:55 +0300

    Мелкие поправки

--- HexrazeMemoryMacro/MemMacro.cpp
+++ HexrazeMemoryMacro/MemMacro.cpp
@@ -1,5 +1,5 @@
 ﻿//........HEXRAZE | GitHub:https://github.com/Barracuda1900 | Discord:barracuda#1479 | Telegram:@barracuda1999 | VK:https://vk.com/hexraze | site:https://hexraze.ru
-//........04.03.2021 C++ ISO 14 
+//........04.03.2021 C++ ISO 14. 
 
 #include <windows.h>
 #include <TlHelp32.h>
